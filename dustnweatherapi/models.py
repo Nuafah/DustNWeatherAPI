f0da68fc74d5b754f61571bb1596aea0b4cc6d65
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-04-20 21:49:57 +0700

    Implement BangkokDust Model

--- dustnweatherapi/models.py
+++ dustnweatherapi/models.py
@@ -18,3 +18,18 @@ class BangkokWeather(models.Model):
     def __str__(self):
             return (f"Weather when {self.ts} in {self.name} will be {self.condition_text}. The temperature is {self.temp_c}"
                     f"that feels like {self.feelslike_c} with the humidity around {self.humidity}%")
+
+
+class BangkokDust(models.Model):
+    """BangkokDust model represents dust data retrieved from Dust Sensor"""
+    ts = models.DateTimeField
+    location = models.CharField(max_length=100)
+    lat = models.DecimalField(decimal_places=2, max_digits=5)
+    lon = models.DecimalField(decimal_places=2, max_digits=5)
+    pm1 = models.DecimalField(decimal_places=2, max_digits=5)
+    pm2_5 = models.DecimalField(decimal_places=2, max_digits=5)
+    pm10 = models.DecimalField(decimal_places=2, max_digits=5)
+
+    def __str__(self):
+        return (f"{self.location} has pm1 = {self.pm1} µ/cubic metre, pm2.5 = {self.pm2_5} "
+                f"µ/cubic metre, pm10 = {self.pm10} µ/cubic metre")
