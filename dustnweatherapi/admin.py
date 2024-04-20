435bd3b9e5a0714228d4816dbbfaf55fcd6a6d81
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-04-20 21:51:46 +0700

    Allow admin to access BangkokDust model

--- dustnweatherapi/admin.py
+++ dustnweatherapi/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 
-from .models import BangkokWeather
+from .models import BangkokWeather, BangkokDust
 admin.site.register(BangkokWeather)
+admin.site.register(BangkokDust)
