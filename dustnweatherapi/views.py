dc1395b498353a8a090274ad99f14f27ee1d80cf
Yasatsawin Kuldejtitipun <yasatsawin.k@ku.th>
2024-05-11 22:35:01 +0700

    Added Integrated Model and Filter options

--- dustnweatherapi/views.py
+++ dustnweatherapi/views.py
@@ -1,11 +1,22 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from .serializers import DustSerializer, WeatherSerializer
-from .models import BangkokWeather, BangkokDust
+from .serializers import DustSerializer, WeatherSerializer, DustNWeatherSerializer
+from .models import BangkokWeather, BangkokDust, BangkokDustNWeather
 
 class WeatherViewSet(viewsets.ModelViewSet):
     queryset = BangkokWeather.objects.all()
     serializer_class = WeatherSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = "__all__"
 
 class DustViewSet(viewsets.ModelViewSet):
     queryset = BangkokDust.objects.all()
     serializer_class = DustSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = "__all__"
+
+class DustNWeatherViewSet(viewsets.ModelViewSet):
+    queryset = BangkokDustNWeather.objects.all()
+    serializer_class = DustNWeatherSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = "__all__"
